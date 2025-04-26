ff562202f926914816c8a06552796afb312db7b4
Your Name <nusrulnakibnahid.mail@gmail.com>
2025-04-26 11:53:50 +0600

    first commit

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:100',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric|between:0,99999999.99',
+            'stock' => 'required|integer|min:0'
+        ]);
+
+        $product = Product::create($validated);
+        return response()->json($product, 201);
+    }
+
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);
+        return response()->json($product);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'sometimes|required|string|max:100',
+            'description' => 'nullable|string',
+            'price' => 'sometimes|required|numeric|between:0,99999999.99',
+            'stock' => 'sometimes|required|integer|min:0'
+        ]);
+
+        $product->update($validated);
+        return response()->json($product);
+    }
+
+    public function destroy($id)
+    {
+        $product = Product::findOrFail($id);
+        $product->delete();
+        return response()->json(null, 204);
+    }
+}
